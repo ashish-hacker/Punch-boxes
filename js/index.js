a7d0edc220d123907909be42d53e969919427b01
Ashish Kumar Panigrahy <akpanigrahy26@gmail.com>
2021-06-12 15:49:26 +0530

    deployment ready

--- js/index.js
+++ js/index.js
@@ -6,15 +6,44 @@ const canvasCtx = canvasElement.getContext('2d');
 let x = canvasElement.width - 185;
 
 let y = 0;
+let y1 = canvasElement.height+30;
 let count = 0;
 let co_ordinates = Array(20).fill({'x':0, 'y':0});
 let co_ordinates1 = Array(20).fill({'x':0, 'y':0});
+let co_ordinates2 = Array(20).fill({'x':0, 'y':0});
+let co_ordinates3 = Array(20).fill({'x':0, 'y':0});
 let dist = 0;
 let max_dist = 0;
 let prev = 0;
+class tile {
+  constructor(x, y, colour, velocity, height, width) {
+    this.x = x;
+    this.y = y;
+    this.colour = colour;
+    this.velocity = velocity;
+    this.h = height;
+    this.w = width;
+  }
+  draw() {
+    canvasCtx.beginPath();
+    canvasCtx.fillStyle = this.colour;
+    canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+    canvasCtx.closePath();
+  }
+  update() {
+    this.draw();
+
+    if(this.y > canvasElement.height+150) {
+      this.y = (Math.floor(Math.random()*50)) - 100;
+      this.colour = colours[Math.floor(Math.random()*6+1)];
+    }
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
 // We'll add this to our control panel later, but we'll save it here so we can
 // call tick() each time the graph runs.
-//const fpsControl = new FPS();
+const fpsControl = new FPS();
 
 // Optimization: Turn off animated spinner after its hiding animation is done.
 const spinner = document.querySelector('.loading');
@@ -29,134 +58,92 @@ function calDistance(a, b) {
   return Math.sqrt((a.x - b.x)*(a.x - b.x) +  (a.y - b.y)*(a.y - b.y));
 }
 
+const colours = ["orange", "red", "green", "blue", "#9f765e", "#df765e", "#ff99ff", "#00ffff", "#66ff33", "#ccff66"];
+let v = { x: 0,
+            y: 16};
+
+const left_tiles = [];
+const right_tiles = [];
+for(let i = 1; i <= 3; i++) {
+  let t = new tile(canvasElement.width*0.1*i, (Math.floor(Math.random()*50)) - 100, colours[Math.floor(Math.random()*colours.length+1)], v, 150, 10);
+  left_tiles.push(t);
+}
+for(let i = 7; i <= 9; i++) {
+  let t = new tile(canvasElement.width*0.1*i, (Math.floor(Math.random()*50)) - 100, colours[Math.floor(Math.random()*colours.length+1)], v, 150, 10);
+  right_tiles.push(t);
+}
+
 function onResults(results) {
   // Hide the spinner.
   document.body.classList.add('loaded');
 
-  // Update the frame rate.
-  fpsControl.tick();
 
   // Draw the overlays.
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
   canvasCtx.drawImage(
       results.image, 0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.fillStyle = 'red';
-  canvasCtx.fillRect(x, y, 100, 100);
+
   canvasCtx.beginPath();
   canvasCtx.fillStyle = 'blue';
   canvasCtx.font = "bold 50px Courier";
   canvasCtx.fillText(`Punches :${count}`, 350, 100);
   canvasCtx.closePath();
-  drawConnectors(
-      canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
-        visibilityMin: 0.65,
-        color: 'white'
-      });
+  const body_parts = [results.poseLandmarks[19], results.poseLandmarks[20], results.poseLandmarks[28], results.poseLandmarks[27]];
   drawLandmarks(
       canvasCtx,
-      Object.values(POSE_LANDMARKS_LEFT)
-          .map(index => results.poseLandmarks[index]),
-      {visibilityMin: 0.65, color: zColor, fillColor: 'rgb(255,138,0)'});
-  drawLandmarks(
-      canvasCtx,
-      Object.values(POSE_LANDMARKS_RIGHT)
-          .map(index => results.poseLandmarks[index]),
-      {visibilityMin: 0.65, color: zColor, fillColor: 'rgb(0,217,231)'});
-  drawLandmarks(
-      canvasCtx,
-      Object.values(POSE_LANDMARKS_NEUTRAL)
-          .map(index => results.poseLandmarks[index]),
-      {visibilityMin: 0.65, color: zColor, fillColor: 'white'});
-  const Ax = results.poseLandmarks[23].x;
-  const Ay = results.poseLandmarks[23].y;
-  const Az = results.poseLandmarks[23].z;
-  const Bx = results.poseLandmarks[25].x;
-  const By = results.poseLandmarks[25].y;
-  const Bz = results.poseLandmarks[25].z;
-  const Cx = results.poseLandmarks[27].x;
-  const Cy = results.poseLandmarks[27].y;
-  const Cz = results.poseLandmarks[27].z;
-  const ABx = Ax - Bx;
-  const ABy = Ay - By;
-  const ABz = Az - Bz;
-  const BCx = Cx - Bx;
-  const BCy = Cy - By;
-  const BCz = Cz - Bz;
-  const dotProduct
-        = ABx * BCx
-          + ABy * BCy
-          + ABz * BCz;
-
-  // Find magnitude of
-  // line AB and BC
-  const magnitudeAB
-        = ABx * ABx
-          + ABy * ABy
-          + ABz * ABz;
-  const magnitudeBC
-        = BCx * BCx
-          + BCy * BCy
-          + BCz * BCz;
-  // Find the cosine of
-  // the angle formed
-  // by line AB and BC
-  var angle = dotProduct;
-  angle /= Math.sqrt(magnitudeAB * magnitudeBC);
-  angle = Math.acos(angle);
-  angle = (angle * 180) / Math.PI;
-  // Print the angle
-  //console.log(Math.abs(angle));
-  //canvasCtx.restore();
-  if(angle <= 80) {
-    drawConnectors(
-        canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
-          visibilityMin: 0.65,
-          color: 'green'
-        });
-  }
-  else {
-    drawConnectors(
-        canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
-          visibilityMin: 0.65,
-          color: 'white'
-        });
+      body_parts,
+      {visibilityMin: 0.55, color: zColor, fillColor: 'rgb(255,138,0)'});
+  // GENERATE TILES
+  // generate left tiles
+  for(let i = 0; i < 3; i++) {
+    left_tiles[i].update();
+    right_tiles[i].update();
   }
 
 
-  y += 12;
-  if(y >= canvasElement.height - 150) {
-    y = 0;
-  }
-  //console.log(y);
+  // FIFO operation
   co_ordinates.shift();
   co_ordinates.push({'x':results.poseLandmarks[19].x, 'y':results.poseLandmarks[19].y});
   co_ordinates1.shift();
   co_ordinates1.push({'x':results.poseLandmarks[20].x, 'y':results.poseLandmarks[20].y});
+  co_ordinates2.shift();
+  co_ordinates2.push({'x':results.poseLandmarks[27].x, 'y':results.poseLandmarks[27].y});
+  co_ordinates3.shift();
+  co_ordinates3.push({'x':results.poseLandmarks[28].x, 'y':results.poseLandmarks[28].y});
 
-  if((results.poseLandmarks[19].x >= ((x-50)/canvasElement.width) && results.poseLandmarks[19].x <= (x+100)/canvasElement.width) &&
-    (results.poseLandmarks[19].y >= (y/canvasElement.height) && results.poseLandmarks[19].y <= (y+100)/canvasElement.height)) {
-    y = -300;
-    count += 1;
-    dist = calDistance(co_ordinates[19], co_ordinates[0]);
+  const co = [co_ordinates, co_ordinates1, co_ordinates2, co_ordinates3];
+  //collisions
+  for(let i = 0; i < body_parts.length; i++) {
+    for(let j = 0; j < 3; j++) {
+      if((body_parts[i].x >= ((left_tiles[j].x-50)/canvasElement.width) && body_parts[i].x <= (left_tiles[j].x+100)/canvasElement.width) &&
+    (body_parts[i].y >= (left_tiles[j].y/canvasElement.height) && body_parts[i].y <= (left_tiles[j].y+100)/canvasElement.height)) {
+        left_tiles[j].y = -100;
+        count += 1;
+        dist = calDistance(co[i][19], co[i][0]);
+        max_dist = Math.max(max_dist, dist);
+        if(dist > 0) prev = dist;
+      }
+    }
+    for(let j = 0; j < 3; j++) {
+      if((body_parts[i].x >= ((right_tiles[j].x-50)/canvasElement.width) && body_parts[i].x <= (right_tiles[j].x+100)/canvasElement.width) &&
+    (body_parts[i].y >= (right_tiles[j].y/canvasElement.height) && body_parts[i].y <= (right_tiles[j].y+100)/canvasElement.height)) {
+        right_tiles[j].y = -100;
+        count += 1;
+        dist = calDistance(co[i][19], co[i][0]);
+        max_dist = Math.max(max_dist, dist);
+        if(dist > 0) prev = dist;
+      }
+    }
   }
-  if((results.poseLandmarks[20].x >= ((x-50)/canvasElement.width) && results.poseLandmarks[20].x <= (x+100)/canvasElement.width) &&
-    (results.poseLandmarks[20].y >= (y/canvasElement.height) && results.poseLandmarks[20].y <= (y+100)/canvasElement.height)) {
-    y = -300;
-    count += 1;
 
-    dist = calDistance(co_ordinates1[19], co_ordinates1[0]);
-  }
-  console.log(count);
   canvasCtx.beginPath();
-  canvasCtx.fillStyle = 'yellow';
+  canvasCtx.fillStyle = 'red';
   canvasCtx.font = "bold 25px Courier";
   //canvasCtx.fillText(`Current Velocity :${(dist/20).toFixed(3)}`, 700, 100);
   canvasCtx.fillText(`Previous Velocity :${(prev/20).toFixed(3)}`, 700, 90);
   canvasCtx.fillText(`Maximum Velocity :${(max_dist/20).toFixed(3)}`, 700, 120);
   canvasCtx.closePath();
-  max_dist = Math.max(max_dist, dist);
-  if(dist > 0) prev = dist;
   dist = 0;
   canvasCtx.restore();
 }
@@ -189,7 +176,7 @@ new ControlPanel(controlsElement, {
     })
     .add([
       new StaticText({title: 'MediaPipe Pose'}),
-      //fpsControl,
+      fpsControl,
       new Toggle({title: 'Selfie Mode', field: 'selfieMode'}),
       new Slider({
         title: 'Model Complexity',
